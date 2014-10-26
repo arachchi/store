769a05e22f204bb6b090f7ce18cb7df95fc3b566
Nuran Rangika Kasthuriarachchi <nurangika@gmail.com>
2014-10-26 16:41:02 +0530

    initial commit

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\opencart/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\opencart/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\opencart/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\opencart/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\opencart/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\opencart/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\opencart/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'M7fuDwtYeu6ys45D');
+define('DB_DATABASE', 'store');
+define('DB_PREFIX', '');
+?>
